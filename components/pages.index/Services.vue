40c14654ce68ea268a3147232aa540d238141eb8
TMook <msp770@gmail.com>
2021-06-03 14:53:22 +0900

    [#10][FEATURE] Services.vue 디자인 확정 및 Timeline.vue 디테일 수정    - 수정이유: 보유 서비스 요약 표시 디자인    - 수정내용: Services.vue 내 EASYXPLAIN 표시법 확정    - 세부내용: TypeScript 미적용 부분 적용

--- components/pages.index/Services.vue
+++ components/pages.index/Services.vue
@@ -1,46 +1,65 @@
 <template>
-  <v-card flat>
-    <v-row justify="center">
-      <v-col v-for="(item, i) in items" :key="i" cols="12" sm="3">
-        <!-- Start: Content -->
-        <v-card
-          flat
-          tile
-          width="100%"
-          :height="cardHeight"
-          class="d-flex justify-center align-center"
-        >
-          <!-- Start: Explanation -->
-          <v-list-item v-if="i === 0" three-line>
-            <v-list-item-content>
-              <div class="d-flex justify-center align-center">
-                {{ item.cardText }}
-              </div>
-            </v-list-item-content>
-          </v-list-item>
+  <v-card :height="height" flat>
+    <v-img
+      src="https://cdn.pixabay.com/photo/2017/03/03/13/56/key-2114046_960_720.jpg"
+      :height="height"
+      class="d-flex align-center"
+    >
+      <v-container>
+        <v-row justify="start">
+          <v-card
+            outlined
+            flat
+            max-width="345"
+            :height="height - 20"
+            class="my-0 ml-5 pa-0"
+          >
+            <!-- Start: Image -->
+            <v-card-actions class="d-flex justify-center mt-4">
+              <v-btn text max-width="100" :href="content.href">
+                <v-img
+                  src="/img/EASYXPLAIN_LOGO.png"
+                  height="25"
+                  contain
+                ></v-img>
+              </v-btn>
+            </v-card-actions>
 
-          <!-- Start: Service Link -->
-          <v-card-actions v-else-if="i === 1">
-            <v-btn text class="ml-2 mr-4 px-0" :to="item.to" :href="item.href">
-              <v-img
-                src="/img/EASYXPLAIN_LOGO.png"
-                alt="EASYXPLAIN"
-                contain
-                height="25"
-                max-width="100"
-              ></v-img>
-            </v-btn>
-          </v-card-actions>
-
-          <!-- Start: Introduce & Regulations -->
-          <div v-else>
-            <v-btn text :to="item.to" :href="item.href">
-              {{ item.cardText }}
-            </v-btn>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
+            <!-- Start: Text -->
+            <v-card-title class="d-flex align-center expCard">
+              <span class="mr-2">
+                {{ content.name }}
+              </span>
+              <v-btn
+                text
+                x-small
+                :color="linkBtnColor"
+                :href="content.shortcut.href"
+              >
+                {{ content.shortcut.name }}
+              </v-btn>
+              <v-btn
+                text
+                x-small
+                :color="linkBtnColor"
+                :to="content.introduce.to"
+              >
+                {{ content.introduce.name }}
+              </v-btn>
+              <v-btn
+                text
+                x-small
+                :color="linkBtnColor"
+                :to="content.regulations.to"
+              >
+                {{ content.regulations.name }}
+              </v-btn>
+            </v-card-title>
+            <v-card-subtitle>{{ content.explanation }}</v-card-subtitle>
+          </v-card>
+        </v-row>
+      </v-container>
+    </v-img>
   </v-card>
 </template>
 
@@ -49,33 +68,33 @@ import { Component, Provide, Vue } from 'nuxt-property-decorator'
 
 @Component({})
 class PagesIndexServices extends Vue {
-  @Provide() cardHeight: Number = 150
-  @Provide() items: Array<{
-    cardText: string
-    href: string | undefined
-    to: string | undefined
-  }> = [
-    {
-      cardText: '전문/기술 용어와 각종 가이드의 가장 쉬운 설명 제공',
-      href: undefined,
-      to: undefined,
-    },
-    {
-      cardText: '바로가기',
+  @Provide() height: string = '200'
+  @Provide() linkBtnColor: string = 'primary'
+  @Provide() content: {
+    name: string
+    explanation: string
+    href: string
+    shortcut: { name: string; href: string }
+    introduce: { name: string; to: string }
+    regulations: { name: string; to: string }
+  } = {
+    name: 'EASYXPLAIN',
+    explanation:
+      '짧고 쉬운 전문/기술용어 설명과 모든 가이드를 얻거나 제공하고 쉽게 설명하는 능력을 인정 받는 플랫폼',
+    href: 'https://frontend.easyxplain.com/',
+    shortcut: {
+      name: '바로가기',
       href: 'https://frontend.easyxplain.com/',
-      to: undefined,
     },
-    {
-      cardText: '소개',
-      href: undefined,
+    introduce: {
+      name: '소개',
       to: '/service/easyxplain/introduce',
     },
-    {
-      cardText: '이용 규정',
-      href: undefined,
+    regulations: {
+      name: '규정',
       to: '/service/easyxplain/regulations',
     },
-  ]
+  }
 }
 
 export default PagesIndexServices
--- components/pages.index/Timeline.vue
+++ components/pages.index/Timeline.vue
@@ -7,7 +7,7 @@
         :left="item.left"
         :right="!item.left"
         :small="item.small"
-        color="secondary"
+        :color="item.color"
       >
         <template #opposite>
           <span
@@ -34,44 +34,55 @@
   </v-card>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      content: [
-        {
-          title: "'EASYXLPLAIN' Alpha Test",
-          date: '2020.10',
-          body:
-            "'EASYXLPLAIN' 최소 작동 모델(MVP) 완성 후, 고객지향적인 서비스 개발을 위해 고객 대상 최초 테스트가 이루어졌습니다.",
-          left: false,
-          small: true,
-        },
-        {
-          title: '쉬운지식 유한책임회사 설립',
-          date: '2020.7',
-          body: '본격적인 발돋움을 위해 법인 설립을 완료하였습니다',
-          left: true,
-          small: false,
-        },
-        {
-          title: '창립 멤버 구성',
-          date: '2019.7',
-          body:
-            '총 4명으로 구성된 창립 멤버로, 우수한 개발 능력을 갖춘 멤버가 합류함으로써 본격적인 서비스 개발에 착수했습니다.',
-          left: true,
-          small: true,
-        },
-        {
-          title: "'EASYXLPLAIN' 최초 개발",
-          date: '2019.4',
-          body:
-            "아이디어 상태였던 'EASYXLPLAIN' 아이템을 직접 개발하기 시작했습니다.",
-          left: false,
-          small: true,
-        },
-      ],
-    }
-  },
+<script lang="ts">
+import { Component, Provide, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+class PagesIndexTimeline extends Vue {
+  @Provide() content: Array<{
+    title: string
+    date: string
+    body: string
+    left: boolean
+    small: boolean
+    color: string
+  }> = [
+    {
+      title: "'EASYXLPLAIN' Alpha Test",
+      date: '2020.10',
+      body:
+        "'EASYXLPLAIN' 최소 작동 모델(MVP) 완성 후, 고객지향적인 기능 개발을 위해 고객 대상 최초 테스트가 이루어졌습니다.",
+      left: false,
+      small: true,
+      color: 'secondary',
+    },
+    {
+      title: '쉬운지식 유한책임회사 설립',
+      date: '2020.7',
+      body: '본격적인 발돋움을 위해 법인 설립을 완료하였습니다',
+      left: true,
+      small: false,
+      color: 'primary',
+    },
+    {
+      title: '창립 멤버 구성',
+      date: '2019.7',
+      body:
+        '총 4명으로 구성된 창립 멤버로, 우수한 개발 능력을 갖춘 멤버가 합류함으로써 본격적인 서비스 개발에 착수했습니다.',
+      left: true,
+      small: true,
+      color: 'secondary',
+    },
+    {
+      title: "'EASYXLPLAIN' 최초 개발",
+      date: '2019.4',
+      body:
+        "아이디어 상태였던 'EASYXLPLAIN' 아이템을 직접 개발하기 시작했습니다.",
+      left: false,
+      small: true,
+      color: 'secondary',
+    },
+  ]
 }
+export default PagesIndexTimeline
 </script>
