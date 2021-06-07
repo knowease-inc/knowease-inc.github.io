cdbb4ea0c9ab6cdfed90cab7cd79bfedb8533e21
TMook <msp770@gmail.com>
2021-06-07 10:42:55 +0900

    [#14][FEATURE] /pages.introduce/Carousel.vue 추가    - 수정이유: 서비스 핵심 소개 필요    - 수정내용: 핵심 소개를 이미지 + 메시지로 표현할 Carousel.vue 추가 및 삽입

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,29 @@
       <!-- ### Start : Top bar ### -->
       <v-app-bar :color="appBar.color" dense flat absolute>
         <v-spacer />
-        <v-toolbar-items>
+        <!-- Start : Knowease image -->
+        <v-toolbar-items v-if="onKnowease">
           <v-btn text to="/" active-class="no-active">
             <v-img
               src="/img/knowease_logo_20210601.png"
               alt="쉬운지식"
+              contain
+              height="20"
+              max-width="100"
+            ></v-img>
+          </v-btn>
+        </v-toolbar-items>
+
+        <!-- Start : Easyxplain image -->
+        <v-toolbar-items v-else>
+          <v-btn
+            text
+            href="https://frontend.easyxplain.com/"
+            active-class="no-active"
+          >
+            <v-img
+              src="/img/EASYXPLAIN_LOGO.png"
+              alt="EASYXPLAIN"
               contain
               height="20"
               max-width="100"
@@ -29,21 +47,35 @@
   </v-app>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Provide, Vue } from 'nuxt-property-decorator'
 import MainFooter from '@/components/layouts.default/MainFooter.vue'
-export default {
+@Component({
   components: {
     MainFooter,
   },
-  data() {
-    return {
-      appBar: {
-        color: 'white',
-        title: '쉬운지식',
-      },
-    }
+  computed: {
+    onKnowease() {
+      const currentRoute: string = this.$route.path
+      if (
+        currentRoute === '/service/easyxplain/introduce' ||
+        currentRoute === '/service/easyxplain/regulations'
+      ) {
+        return false
+      } else {
+        return true
+      }
+    },
   },
+})
+class LayoutDefault extends Vue {
+  @Provide() appBar: { color: string; title: string } = {
+    color: 'white',
+    title: '쉬운지식',
+  }
 }
+
+export default LayoutDefault
 </script>
 
 <style scoped>
