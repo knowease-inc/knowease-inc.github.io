ed5e4d54cf3b70d066a68d086e7ddedf987c4cd6
TMook <msp770@gmail.com>
2021-06-08 17:32:41 +0900

    [#11][REFACTOR] HowToUseCards.vue icon 이미지 경로 수정 및 font 디테일 수정   - 수정이유: /static/img 경로 일치 및 font 디테일 변경 필요   - 수정내용: /static/img/ 내 폴더 경로 동일화   - 세부내용: font-weight 및 avatar size 변경

--- components/pages.service.easyxplain.introduce/HowToUseCards.vue
+++ components/pages.service.easyxplain.introduce/HowToUseCards.vue
@@ -6,9 +6,9 @@
           <v-card flat outlined class="ma-1">
             <v-list-item three-line class="pl-1 pr-2">
               <!-- Start: Image -->
-              <v-list-item-avatar size="80" class="mx-0">
+              <v-list-item-avatar :size="avatarSize" class="mx-0">
                 <v-avatar tile>
-                  <img :src="item.image" />
+                  <v-img :src="item.image" />
                 </v-avatar>
               </v-list-item-avatar>
 
@@ -34,6 +34,7 @@
 import { Component, Provide, Vue } from 'nuxt-property-decorator'
 @Component({})
 class PagesIntroduceHowToUseCards extends Vue {
+  @Provide() avatarSize: string = '80px'
   @Provide() items: Array<{
     title: string
     detail: string
@@ -42,27 +43,32 @@ class PagesIntroduceHowToUseCards extends Vue {
     {
       title: '필요 정보 찾기',
       detail: '전문/기술 용어의 설명 또는 쉽게 작성된 가이드를 찾아보세요',
-      image: '/img/search_icon.png',
+      image:
+        '/img/pages.service.easyxplain.introduce.HowToUseCards/search_icon.png',
     },
     {
       title: "이거 '쉬워요' 누르기",
       detail: "정말 쉽게 작성된 설명에는 '쉬워요'를 주세요!",
-      image: '/img/lightbulb_icon.png',
+      image:
+        '/img/pages.service.easyxplain.introduce.HowToUseCards/lightbulb_icon.png',
     },
     {
       title: '쉬운 설명 작성',
       detail: '가이드 또는 전문/기술 용어 설명을 직접 쉽게 써주세요',
-      image: '/img/chat_icon.png',
+      image:
+        '/img/pages.service.easyxplain.introduce.HowToUseCards/chat_icon.png',
     },
     {
       title: '가이드 주제/용어 등록',
       detail: '찾는 전문/기술 용어, 가이드 주제가 없다면 등록해주세요',
-      image: '/img/add_icon.png',
+      image:
+        '/img/pages.service.easyxplain.introduce.HowToUseCards/add_icon.png',
     },
     {
       title: '업적 달성 및 인증',
       detail: '다양한 업적을 달성, 설명 능력이 객관적 인증됩니다',
-      image: '/img/id_icon.png',
+      image:
+        '/img/pages.service.easyxplain.introduce.HowToUseCards/id_icon.png',
     },
   ]
 }
@@ -71,6 +77,6 @@ export default PagesIntroduceHowToUseCards
 
 <style scoped>
 .contentTitle {
-  font-weight: 700;
+  font-weight: 600;
 }
 </style>
