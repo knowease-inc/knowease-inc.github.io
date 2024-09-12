ad90259f12dabbc4ad30f82378acddad654bb723
tpdud406 <tpdud406@gmail.com>
2024-09-12 15:23:41 +0900

    [#182][FEATURE] 연혁 설명 문구 변경   - 수정이유: 고객 대상층 구체화 및 포괄적이고 명확하게 수정   - 수정내용: 연혁 내부 문구 변경

--- components/pages.index/Timeline.vue
+++ components/pages.index/Timeline.vue
@@ -100,7 +100,7 @@ export default {
           title: '파일럿 프로그램 진행',
           date: '2024.4',
           body:
-            '증권업 마케팅 부문, 화장품 제조업 상품기획 실무용 트렌드 브리핑 작성 AI 테스트 참여 제공 및 서비스 개선 실시',
+            '상장/대기업 실무자 대상 맞춤형 트렌드 브리핑 작성 AI 테스트 참여 제공 및 AI 개선 실시',
           left: false,
           small: true,
           color: 'primary',
