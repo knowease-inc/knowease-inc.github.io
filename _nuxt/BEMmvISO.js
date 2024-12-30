47fac6e17e424d44b6d6fc941ac2012fa58019e9
T-Mook <T-Mook@users.noreply.github.com>
2024-12-30 11:31:52 +0000

    deploy: 05a3cdafbf4c9f04bac7ae28c66ac7d05230f3d7

---
+++ _nuxt/BEMmvISO.js
@@ -0,0 +1,1 @@
+import{p as e,af as r}from"./B-DO_XeL.js";const t=e({class:[String,Array,Object],style:{type:[String,Array,Object],default:null}},"component");function n(e){r("useRender").render=e}export{t as m,n as u};
