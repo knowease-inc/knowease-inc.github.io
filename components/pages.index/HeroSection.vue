df9d81e8a1371955c47d97aeafcaa4b2e7db05c5
tpdud406 <tpdud406@gmail.com>
2025-01-14 14:57:58 +0900

    [#221][FEATURE][REFACTOR] 신규 디자인 적용(top bar, hero section)   - 수정이유: 트렌드를 반영한 신규 디자인 적용 필요   - 수정내용: top bar, hero section 컴포넌트에 신규 디자인 적용

---
+++ components/pages.index/HeroSection.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-row justify="center" class="px-0">
+    <v-card
+      width="100%"
+      height="100vh"
+      class="image-container d-flex flex-column justify-end"
+    >
+      <!-- Text Contents -->
+      <v-col cols="10" offset="1" offset-lg="2" sm="10" class="px-4 text-box">
+        <p
+          :style="upperTextStyle"
+          class="font-weight-black"
+          v-html="t('pages.index.hero.upper')"
+        />
+        <p
+          :style="lowerTextStyle"
+          class="mt-4 mt-sm-0"
+          v-html="lowerTextContent"
+        />
+      </v-col>
+    </v-card>
+  </v-row>
+</template>
+
+<script setup>
+const { xs, smAndUp, mdAndUp, lgAndUp } = useDisplay()
+const { t, locale } = useI18n()
+
+// Upper Text Style
+const upperTextStyle = computed(() => ({
+  lineHeight: xs.value ? '1.4' : '1.2',
+  fontSize: xs.value ? '1.8rem' : lgAndUp.value ? '6rem' : '3.5rem',
+  fontWeight: xs.value ? '500' : '700',
+}))
+
+// Lower Text Style
+const lowerTextStyle = computed(() => ({
+  lineHeight: '1.4',
+  fontSize: xs.value ? '0.9rem' : smAndUp.value ? '1.4rem' : '1.8rem',
+  fontWeight: xs.value ? '500' : '700',
+  paddingBottom: xs.value ? '30%' : '20%',
+}))
+
+// Lower Text Content with Specific Breaks
+const lowerTextContent = computed(() => {
+  const text = t('pages.index.hero.lower')
+
+  if (xs.value) {
+    // 예: 특정 단어에서 줄바꿈 처리
+
+    return locale.value === 'EN'
+      ? text.replace('technology,', 'technology,<br />')
+      : text.replace('곳으로', '곳으로<br />')
+  }
+
+  return text
+})
+</script>
+
+<style scoped>
+.text-box {
+  color: white;
+  height: auto; /* 내부 콘텐츠 크기만큼 */
+  max-height: fit-content; /* 높이 제한 */
+}
+
+.image-container {
+  background: url('/img/new/heroImg.jpg') no-repeat center center;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
