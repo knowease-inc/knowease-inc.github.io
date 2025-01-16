cb6a167e7efc0ec8884277349a130ede31aa9705
tpdud406 <tpdud406@gmail.com>
2025-01-16 20:04:48 +0900

    [#221][FIX] 다크모드 토글 버튼 오류 수정   - 수정이유: 상위, 하위 컴포넌트간 status 동기화 실패로 에러 발생   - 수정내용: 하위 변수 삭제 후 props받은 상위 변수 사용으로 변경

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,39 +16,44 @@ import { useTheme } from 'vuetify'
 import TopBar from '@/components/layouts.default/TopBar.vue'
 import MainFooter from '@/components/layouts.default/MainFooter.vue'
 
+const { global: theme } = useTheme()
+const { trackEvent } = useGA4()
+
 // 다크 모드 상태 관리
 const isDark = ref(false)
-
-const { global: theme } = useTheme()
 
 // 다크 모드 토글 함수
 const toggleDarkMode = () => {
   isDark.value = !isDark.value
   theme.name.value = isDark.value ? 'dark' : 'light' // 테마 상태 업데이트
   localStorage.setItem('theme', theme.name.value) // 상태 저장
+
+  // GA4 이벤트 전송
+  trackEvent('dark_mode_toggle', {
+    dark_mode_status: isDark.value ? 'enabled' : 'disabled',
+  })
 }
 
+if (typeof window !== 'undefined') {
+  const script = document.createElement('script')
 
-if (typeof window !== "undefined") {
-  const script = document.createElement("script");
-
-  script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js";
+  script.async = true
+  script.src = 'https://www.googletagmanager.com/gtag/js'
   script.onload = () => {
-    window.dataLayer = window.dataLayer || [];
+    window.dataLayer = window.dataLayer || []
 
     function gtag() {
-      dataLayer.push(arguments);
+      dataLayer.push(arguments)
     }
 
-    window.gtag = gtag; // 전역 함수(gtag) 설정
+    window.gtag = gtag // 전역 함수(gtag) 설정
 
-    gtag("js", new Date());
+    gtag('js', new Date())
 
-    console.log("Google Analytics initialized");
-  };
+    console.log('Google Analytics initialized')
+  }
 
-  document.head.appendChild(script);
+  document.head.appendChild(script)
 }
 
 // 로컬 스토리지에서 테마 상태 복원
