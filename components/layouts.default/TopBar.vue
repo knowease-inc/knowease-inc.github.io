d8c133409b87b1b3c319259e89c8d54fa4bc201b
tpdud406 <tpdud406@gmail.com>
2025-01-31 17:21:59 +0900

    [#221][REFACTOR] 정식 이미지로 교체 및 자잘한 디자인 수정 및 레이아웃 충돌 해소   - 수정이유: 라이선스 문제 예방, 로고와 내용이 충돌하는 수정, 디자인상 수정   - 수정내용: 세부사항 참조   - 세부사항:     - index페이지: creattie 정식 gif로 교체                  : 레이어 겹쳐보이는 이미지 교체                  : 디자인상 '그림자'유무에 따른 수정     - '고객문의', '개인정보' 페이지: 콘텐츠와 상단 로고 충돌 방지를 위한 수정     - topbar: 페이지 및 상황에 따른 글자, 로고색 적용

--- components/layouts.default/TopBar.vue
+++ components/layouts.default/TopBar.vue
@@ -17,7 +17,7 @@
         <!-- Logo -->
         <v-btn text to="/" color="transparent" class="px-0">
           <v-img
-            :src="appBar.knowease.imgSrc"
+            :src="logoSrc"
             :alt="appBar.knowease.title"
             :height="smAndUp ? appBar.height : '20'"
             :width="sm ? 80 : appBar.maxWidth"
@@ -33,7 +33,7 @@
             :key="i"
             :size="sm ? 'small' : 'large'"
             variant="text"
-            color="white"
+            :color="textColor"
             selected-class="no-active"
             class="font-weight-bold"
             @click="navigateWithTracking(item)"
@@ -53,18 +53,18 @@
           <template v-slot:activator="{ props }">
             <v-app-bar-nav-icon
               v-bind="props"
-              color="white"
+              :color="isIndexPage ? 'white' : ''"
               class="pb-2"
               :icon="isMenuOpen ? mdiClose : mdiMenu"
             />
           </template>
 
           <!-- Start: Menu Contents -->
-          <v-card class="mx-sm-n5 pa-4 mt-n1" color="black" tile>
+          <v-card class="mx-n5 pa-4 mt-n1" variant="tile">
             <v-list
               :style="{
                 'background-color': 'transparent',
-                color: 'white',
+                color: textColor,
               }"
               width="100%"
               class="font-weight-bold"
@@ -81,11 +81,11 @@
             <!-- Start: Language change & DarkMode Switch -->
             <v-col cols="12" class="pb-0">
               <!-- Start: Language change Btsns -->
-              <v-icon :icon="mdiWeb" color="white" size="small" />
+              <v-icon :icon="mdiWeb" :color="textColor" size="small" />
 
               <v-btn
                 style="font-size: 0.9rem"
-                color="white"
+                :color="textColor"
                 variant="text"
                 class="font-weight-bold"
                 size="small"
@@ -96,7 +96,7 @@
 
               <v-btn
                 style="font-size: 0.9rem"
-                color="white"
+                :color="textColor"
                 variant="text"
                 class="font-weight-bold"
                 size="small"
@@ -112,11 +112,11 @@
               <v-btn
                 :prepend-icon="mdiLaptop"
                 rounded="lg"
-                color="white"
+                :color="textColor"
                 variant="outlined"
                 @click.prevent="toggleDarkMode()"
               >
-                {{ isDark ? 'Light' : 'Dark' }}
+                {{ props.isDark ? 'Light' : 'Dark' }}
               </v-btn>
             </v-col>
           </v-card>
@@ -125,11 +125,11 @@
         <!-- Start: Language change & DarkMode Switch -->
         <v-col v-if="smAndUp" cols="2" class="d-flex align-center justify-end">
           <!-- Start: Language change Btsns -->
-          <v-icon :icon="mdiWeb" color="white" />
+          <v-icon :icon="mdiWeb" :color="textColor" />
 
           <v-btn
             style="font-size: 1rem"
-            color="white"
+            :color="textColor"
             variant="text"
             class="font-weight-bold"
             @click="trackAndSetLanguage('ko')"
@@ -139,7 +139,7 @@
 
           <v-btn
             style="font-size: 1rem"
-            color="white"
+            :color="textColor"
             variant="text"
             class="font-weight-bold"
             @click="trackAndSetLanguage('en')"
@@ -152,12 +152,12 @@
           <v-btn
             :prepend-icon="mdiLaptop"
             rounded="lg"
-            color="white"
+            :color="textColor"
             size="large"
             variant="outlined"
             @click.prevent="toggleDarkMode()"
           >
-            {{ isDark ? 'Light' : 'Dark' }}
+            {{ props.isDark ? 'Light' : 'Dark' }}
           </v-btn>
         </v-col>
       </v-col>
@@ -174,7 +174,9 @@ const router = useRouter()
 const { t, locale, setLocale } = useI18n() // 현재 언어(locale)와 언어 변경(setLocale) 가져오기
 const { trackEvent } = useGA4()
 
-defineProps(['isDark'])
+const props = defineProps({
+  isDark: Boolean,
+})
 
 const emit = defineEmits(['toggle-dark-mode'])
 
@@ -186,6 +188,7 @@ const appBar = {
   knowease: {
     title: '(주)게타(GET-A Inc.)',
     imgSrc: '/img/new/GET_A_LOGO_White.png',
+    colorImgSrc: '/img/GET_A_LOGO.png',
   },
   easyxplain: {
     title: '미닛(MEANIIT)',
@@ -193,6 +196,24 @@ const appBar = {
   },
 }
 const isMenuOpen = ref(false)
+
+const isIndexPage = computed(() => route.path === '/')
+
+// "텍스트 색" (다크모드 & index 페이지 여부 반영)
+const textColor = computed(() => {
+  if (props.isDark) return 'white'
+  if (xs.value) return 'black'
+  return isIndexPage.value ? 'white' : 'black'
+})
+
+// "로고 이미지 색" (다크모드 & index 페이지 여부 반영)
+const logoSrc = computed(() => {
+  if (props.isDark) return appBar.knowease.imgSrc
+
+  return isIndexPage.value
+    ? appBar.knowease.imgSrc
+    : appBar.knowease.colorImgSrc
+})
 
 const menuItems = computed(() => [
   {
